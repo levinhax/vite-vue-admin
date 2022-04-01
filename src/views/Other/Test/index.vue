87050d500f2f18b663e8d7e5e433daf4939384b0
levinhax <levin1229@163.com>
2022-04-01 14:58:50 +0800

    feat: 引入VueUse

--- src/views/Other/Test/index.vue
+++ src/views/Other/Test/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { useMouse, useDebounceFn, useThrottleFn } from '@vueuse/core'
 import { useUserStore } from '../../../store/modules/user'
+
 const userStore = useUserStore()
 const message = ref('hello levin')
 
@@ -10,6 +12,33 @@ const handleUpdateUser = () => {
     userName: 'sysAdmin',
     phone: '13909878899',
   })
+}
+
+// tracks mouse position
+const { x, y } = useMouse()
+
+const updated1 = ref(0)
+const clicked1 = ref(0)
+const debouncedFn = useDebounceFn(
+  () => {
+    updated1.value += 1
+  },
+  1000,
+  { maxWait: 5000 }
+)
+const clickedFn1 = () => {
+  clicked1.value += 1
+  debouncedFn()
+}
+
+const updated2 = ref(0)
+const clicked2 = ref(0)
+const throttledFn = useThrottleFn(() => {
+  updated2.value += 1
+}, 1000)
+const clickedFn2 = () => {
+  clicked2.value += 1
+  throttledFn()
 }
 </script>
 
@@ -23,6 +52,25 @@ const handleUpdateUser = () => {
     <a-divider orientation="left">全局组件</a-divider>
     <div>
       <com-search-bar></com-search-bar>
+    </div>
+    <a-divider orientation="left">VueUse</a-divider>
+    <div>
+      <div>useMouse pos: {{ x }}, {{ y }}</div>
+      <div>
+        <a-button @click="clickedFn1">Smash me! (防抖)</a-button>
+        <span>Delay is set to 1000ms and maxWait is set to 5000ms for this demo.</span>
+
+        <p>Button clicked: {{ clicked1 }}</p>
+        <p>Event handler called: {{ updated1 }}</p>
+      </div>
+
+      <div>
+        <a-button @click="clickedFn2"> Smash me! (节流)</a-button>
+        <span>Delay is set to 1000ms for this demo.</span>
+
+        <p>Button clicked: {{ clicked2 }}</p>
+        <p>Event handler called: {{ updated2 }}</p>
+      </div>
     </div>
   </div>
 </template>
