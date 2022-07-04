7085b8d0c2e23fb35b1a7cd28f768231b42c93eb
levinhax <levin1229@163.com>
2022-07-04 19:25:45 +0800

    feat: pdf多页渲染

--- src/views/Other/File/Preview/component/pdfView.vue
+++ src/views/Other/File/Preview/component/pdfView.vue
@@ -15,6 +15,7 @@ import pdfFile from '../../../../../assets/file/前端新工具-Vite.pdf'
 import * as pdfJs from 'pdfjs-dist/legacy/build/pdf'
 // import * as pdfJsWorkerSrc from 'pdfjs-dist/legacy/build/pdf.worker.entry.js'
 import pdfJsWorkerSrc from 'pdfjs-dist/legacy/build/pdf.worker.entry.js'
+import { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api'
 // 设置pdf.worker.js文件的引入地址
 // pdfJs.GlobalWorkerOptions.workerSrc = require('pdfjs-dist/legacy/build/pdf.worker.entry.js')
 pdfJs.GlobalWorkerOptions.workerSrc = pdfJsWorkerSrc
@@ -33,7 +34,8 @@ const loadingPdf = ref<boolean>(false)
 const pdfUrl = ref<string>('') // pdf的base64
 const pdfDoc = ref<any>(null) // pdfjs 生成的对象
 const pdfPageNum = ref<number>(0) // 当前页数
-const pdfPages = ref([])
+const pdfPages = ref<number>(0)
+const pdfScale = ref(1)
 
 // const renderPage = (num: number, pdfDoc: any) => {
 //   pdfDoc.getPage(num).then(function (pdfPage: any) {
@@ -55,36 +57,54 @@ const renderPdf = (url: any) => {
   console.log('renderPdf url: ', url)
   // data是一个ArrayBuffer格式，也是一个buffer流的数据
   pdfJs.getDocument(url).promise.then(pdfDoc => {
-    console.log('pdfDoc: ', pdfDoc)
-    return pdfDoc.getPage(1).then(function (pdfPage) {
-      // Display page on the existing canvas with 100% scale.
-      const viewport = pdfPage.getViewport({ scale: 2.0 })
-      console.log('viewport: ', viewport)
-      let canvas: any = toRaw(refPdfCanvas.value)
-      if (canvas == null) return
-      canvas.width = viewport.width
-      canvas.height = viewport.height
-      const ctx = canvas.getContext('2d')
-      const renderTask = pdfPage.render({
-        canvasContext: ctx,
-        viewport,
-      })
-      return renderTask.promise
-    })
+    // console.log('pdfDoc: ', pdfDoc._pdfInfo.numPages)
+    pdfPages.value = pdfDoc._pdfInfo.numPages
+    // return pdfDoc.getPage(1).then(function (pdfPage) {
+    //   let canvas: any = toRaw(refPdfCanvas.value)
+    //   if (canvas == null) return
+    //   const ctx = canvas.getContext('2d')
+    //   // Display page on the existing canvas with 100% scale.
+
+    //   const dpr = window.devicePixelRatio || 1
+    //   const bsr =
+    //     ctx.webkitBackingStorePixelRatio ||
+    //     ctx.mozBackingStorePixelRatio ||
+    //     ctx.msBackingStorePixelRatio ||
+    //     ctx.oBackingStorePixelRatio ||
+    //     ctx.backingStorePixelRatio ||
+    //     1
+    //   const ratio = dpr / bsr
+
+    //   const viewport = pdfPage.getViewport({ scale: pdfScale.value * 2 })
+    //   canvas.width = viewport.width * ratio
+    //   canvas.height = viewport.height * ratio
+    //   canvas.style.width = viewport.width + 'px'
+    //   canvas.style.height = viewport.height + 'px'
+    //   console.log('viewport: ', viewport)
+    //   ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
+    //   const renderTask = pdfPage.render({
+    //     canvasContext: ctx,
+    //     viewport,
+    //   })
+    //   return renderTask.promise
+    // })
+
+    for (let num = 1; num <= pdfPages.value; num++) {
+      renderPage(num, pdfDoc)
+    }
   })
 }
 
 // 渲染pdf
 
-const renderPage = async (num = 1) => {
-  pdfPageNum.value = num
+const renderPage = async (num = 1, pdfDoc: PDFDocumentProxy) => {
   //渲染pdf页
   console.log('渲染pdf页')
-  const page = await pdfDoc.value.getPage(num)
-  const canvas = document.getElementById('pdfCanvas' + num)
-  // const canvas = document.getElementById("the_canvas");
-  if (!canvas) return
+  const pdfPageRender = await pdfDoc.getPage(num)
+  let canvas: any = toRaw(refPdfCanvas.value)
+  if (canvas == null) return
   const ctx = canvas.getContext('2d')
+
   const dpr = window.devicePixelRatio || 1
   const bsr =
     ctx.webkitBackingStorePixelRatio ||
@@ -94,23 +114,19 @@ const renderPage = async (num = 1) => {
     ctx.backingStorePixelRatio ||
     1
   const ratio = dpr / bsr
-  const viewport = page.getViewport({ scale: this.pdf_scale })
+
+  const viewport = pdfPageRender.getViewport({ scale: pdfScale.value * 2 })
   canvas.width = viewport.width * ratio
   canvas.height = viewport.height * ratio
   canvas.style.width = viewport.width + 'px'
-  this.pdf_div_width = viewport.width + 'px'
   canvas.style.height = viewport.height + 'px'
-  ctx.setTransform(ratio, 0, 0, ratio, 0, 0)
-  const renderContext = {
+  console.log('viewport: ', viewport)
+  ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
+  const renderTask = pdfPageRender.render({
     canvasContext: ctx,
-    viewport: viewport,
-  }
-  page.render(renderContext)
-  if (this.pdf_pages > num) {
-    setTimeout(() => {
-      return this.renderPage(num + 1)
-    })
-  }
+    viewport,
+  })
+  return renderTask.promise
 }
 
 const loadFile = async (url: any) => {
@@ -119,17 +135,17 @@ const loadFile = async (url: any) => {
   console.log(url)
   pdfDoc.value = await pdfJs.getDocument(url).promise
   pdfPages.value = pdfDoc.value.numPages
-  renderPage()
+  // renderPage(1, pdfDoc)
 }
 
 onMounted(() => {
   console.log('onMounted')
   renderPdf(pdfFile)
-  //   pdfJs.getDocument(pdfFile).promise.then(pdfDoc => {
-  //     for (var i = 1; i <= pdfDoc.numPages; i++) {
-  //       renderPage(i, pdfDoc)
-  //     }
-  //   })
+  pdfJs.getDocument(pdfFile).promise.then(pdfDoc => {
+    console.log('pdfDoc: ', pdfDoc._pdfInfo)
+    pdfPages.value = 10
+    renderPage(1, pdfDoc)
+  })
 })
 
 watch(props, (newValue, oldValue) => {
